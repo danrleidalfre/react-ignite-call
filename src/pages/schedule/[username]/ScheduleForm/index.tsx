31daa5d57538453f7d356ad0a0afa5196d358826
Danrlei <danrleidalfre@gmail.com>
2024-05-06 20:23:59 -0300

    add conditionally to select date and confirm schedule

--- src/pages/schedule/[username]/ScheduleForm/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -1,5 +1,22 @@
 import { CalendarStep } from '@/pages/schedule/[username]/ScheduleForm/CalendarStep'
+import { useState } from 'react'
+import { ConfirmStep } from '@/pages/schedule/[username]/ScheduleForm/ConfirmStep'
 
 export function ScheduleForm() {
-  return <CalendarStep />
+  const [selectedDateTime, setSelectedDateTime] = useState<Date | null>()
+
+  function handleClearSelectedDateTime() {
+    setSelectedDateTime(null)
+  }
+
+  if (selectedDateTime) {
+    return (
+      <ConfirmStep
+        schedulingDate={selectedDateTime}
+        onCancelConfirmation={handleClearSelectedDateTime}
+      />
+    )
+  }
+
+  return <CalendarStep onSelectDateTime={setSelectedDateTime} />
 }
